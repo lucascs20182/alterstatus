431c7f714e92bec27499c08586d5de6806e3585e
Sthephanie Soares <sthephaniesoares84@gmail.com>
2021-07-22 14:43:19 -0300

    Home atualizada

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,94 +1,120 @@
-import React from "react";
-import PropTypes from "prop-types";
-import classNames from "classnames";
-import { withStyles } from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import List from "@material-ui/core/List";
-import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import { Button, ListItem } from "@material-ui/core";
-const drawerWidth = 240;
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 
-const styles = theme => ({
+const drawerWidth = 220;
+
+const useStyles = makeStyles((theme) => ({
   appFrame: {
     zIndex: 1,
     overflow: "hidden",
     height: "100vh"
   },
+
   appBar: {
     position: "fixed",
     width: "100%",
-    zIndex: 1400
+    zIndex: 1200,
   },
+
   menuButton: {
     marginLeft: 12,
-    marginRight: 20
+    marginRight: 15
   },
+
   drawerPaper: {
     position: "fixed",
     width: drawerWidth,
-    borderRadius: 0,
-    borderTop: "none",
-    borderBottom: "none",
-    top: theme.spacing(8), // push content down to fix scrollbar position
-    height: `calc(100% - ${theme.spacing(8)}px)` // subtract appbar height
+    borderRadius: 3,
+    top: theme.spacing(7), 
+    height: `calc(100% - ${theme.spacing(8)}px)` 
   },
+
   drawerContent: {
     overflow: "auto",
     display: "flex",
     flexDirection: "column"
   },
+
   contentWrapper: {
     overflow: "auto",
     position: "fixed",
-    top: theme.spacing.unit * 8,
+    top: theme.spacing(6),
     height: "calc(100% - 64px)", // Subtract width of header
     backgroundColor: theme.palette.background.default,
     transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen
     })
+
   },
+
   "content-left": {
     marginLeft: drawerWidth
   },
+
   contentShift: {
     transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen
     })
   },
+
   content: {
     padding: theme.spacing(3)
+
   },
-  logoutContainer: {
-    marginTop: "auto",
-    paddingBottom: "0",
-    position: "fixed",
-    width: drawerWidth,
-    bottom: 0
-  }
-});
+}));
 
-class PersistentDrawer extends React.Component {
-  state = {
-    open: true
+export default function PersistentDrawerLeft() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(!open);
   };
 
-  handleDrawerToggle = () => {
-    this.setState({ open: !this.state.open });
-  };
-
-  render() {
-    const { classes } = this.props;
-    const { open } = this.state;
-
-    const drawer = (
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        elevation={0}
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar disableGutters={true}>
+          <IconButton
+            color="secondary"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Persistent drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
       <Drawer
+        className={classes.drawer}
         variant="persistent"
         anchor="left"
         open={open}
@@ -97,66 +123,27 @@ class PersistentDrawer extends React.Component {
           variant: "outlined"
         }}
         classes={{
-          paper: classes.drawerPaper
+          paper: classes.drawerPaper,
         }}
       >
-        <div className={classes.drawerContent}>
-          <List className={classes.drawerList} style={{ flexGrow: 1 }}>
-            <ListItem>
-              <button variant="display3">Dash</button>
-            </ListItem>
-            <ListItem> 
-              <Typography variant="display3">Dash</Typography>
-            </ListItem>
-            <ListItem>
-              <Typography variant="display3">Dash</Typography>
-            </ListItem>
-            <ListItem>
-              <Typography variant="display3">Dash</Typography>
-            </ListItem>
-            <ListItem>
-              <Typography variant="display3">Dash</Typography>
-            </ListItem>
-          </List>
-          <List className={classes.logoutContainer}>
-            <Divider />
-            <ListItem
-              style={{
-                display: "flex",
-                justifyContent: "center"
-              }}
-            >
-              <Button>Logout</Button>
-            </ListItem>
-          </List>
+        <div className={classes.drawerHeader}>
         </div>
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
       </Drawer>
-    );
-
-    return (
-      <div className={classes.appFrame}>
-        {drawer}
-        <AppBar className={classes.appBar} elevation={0}>
-          <Toolbar disableGutters={true}>
-            <IconButton
-              color="inherit"
-              aria-label="Open drawer"
-              onClick={this.handleDrawerToggle}
-              className={classes.menuButton}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="title" color="inherit" noWrap>
-              Persistent drawer
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <div
-          className={classNames(classes.contentWrapper, {
-            [classes.contentShift]: open,
-            [classes[`content-left`]]: open
-          })}
-        >
+      <main>
+        <div 
+          className={clsx(classes.contentWrapper, {
+          [classes.contentShift]: open,
+          [classes[`content-left`]]: open
+          })} >
           <div className={classes.content}>
             <Typography variant="display4">
               {
@@ -180,14 +167,8 @@ class PersistentDrawer extends React.Component {
             </Typography>
           </div>
         </div>
-      </div>
-    );
-  }
+
+      </main>
+    </div>
+  );
 }
-
-PersistentDrawer.propTypes = {
-  classes: PropTypes.object.isRequired,
-  theme: PropTypes.object.isRequired
-};
-
-export default withStyles(styles, { withTheme: true })(PersistentDrawer);
