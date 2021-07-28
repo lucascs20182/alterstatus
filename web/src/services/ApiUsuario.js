4b70f5ecc756853b70e0b62b58180c51c953e1af
Lucas Cruz <lucascs20182@gmail.com>
2021-07-28 10:33:38 -0300

    crie uma branch para o deploy do back

--- web/src/services/ApiUsuario.js
+++
@@ -1,9 +0,0 @@
-import api from './api';
-
-export function logar(username, senha) {
-    return new Promise((resolve, reject) => {
-        return api.post('/login', { username, senha })
-            .then(response => resolve(response))
-            .catch(error => reject(error))
-    });
-}
