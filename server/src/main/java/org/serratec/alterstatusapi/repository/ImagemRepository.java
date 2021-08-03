a3f593822970f803bc002de6c8b6151ab39c9558
André Ricardo Adami Drummond Filho <81425108+BahPendragon@users.noreply.github.com>
2021-08-03 08:06:18 -0300

    Delete server directory

--- server/src/main/java/org/serratec/alterstatusapi/repository/ImagemRepository.java
+++
@@ -1,9 +0,0 @@
-package org.serratec.alterstatusapi.repository;
-
-import org.serratec.alterstatusapi.model.Imagem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImagemRepository extends JpaRepository<Imagem, Long> {
-
-	Imagem findByUsuarioId(Long id);
-}
