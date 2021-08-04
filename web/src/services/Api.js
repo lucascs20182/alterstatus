fb40ee2c0e1cb32ad63828f954211726c033aecd
Lucas Cruz <lucascs20182@gmail.com>
2021-08-03 21:55:19 -0300

    mude a baseurl para api online no heroku

--- web/src/services/Api.js
+++ web/src/services/Api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { obterTokenDaStorage } from '../utils/Storage';
 
 const api = axios.create({
-//  baseURL: 'https://alterstatus.herokuapp.com'
-  baseURL: 'http://localhost:8080'
+  baseURL: 'https://alterstatus.herokuapp.com'
+//  baseURL: 'http://localhost:8080'
 })
 
 api.interceptors.request.use((config) => {
